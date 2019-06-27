6dbb7e1ace269383328168255f97be1e88ebf1aa
wanghuibin <wanghuibin_zb@cmos.chinamobile.com>
2019-06-27 16:56:00 +0800

    使用axios请求easy-mock数据

--- src/SaleProduct.js
+++ src/SaleProduct.js
@@ -1,15 +1,54 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 
 class SaleProduct extends Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentWillMount(){
+    console.log("组件将要渲染");
+  }
+  componentDidMount(): void {
+    console.log("组件渲染完成");
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if(nextProps.content !== this.props.content){
+      return true
+    } else {
+      return false
+    }
+  }
+  componentWillReceiveProps(nextProps, nextContext) {
+    console.log("将要接收一个新的props");
+  }
+  componentWillUpdate(nextProps, nextState) {
+    //在组件接收到新的props或者state但还没有render时被调用
+    console.log('在组件接收到新的props或者state但还没有render时被调用');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    //在组件完成更新后立即调用。在初始化时不会被调用。
+    console.log('在组件完成更新后立即调用。在初始化时不会被调用。')
+  }
+  componentWillUnmount() {
+    //在组件从 DOM 中移除之前立刻被调用。
+    console.log('在组件从 DOM 中移除之前立刻被调用。')
+  }
+  handleClick(){
+    console.log(this.props.index);
+    this.props.deleteItem(this.props.index);
   }
   render() {
     return (
-      <li></li>
+      <li onClick={this.handleClick}>{this.props.content}</li>
     )
   }
 }
 
+SaleProduct.propTypes = {
+  content:PropTypes.string,
+  deleteItem:PropTypes.func,
+  index:PropTypes.number
+};
 
 export default SaleProduct
--- src/SaleShop.js
+++ src/SaleShop.js
@@ -1,30 +1,74 @@
 import React, {Component} from 'react';
 import {Button, Input, Tag} from "antd";
 import './App.css'
+import SaleProduct from "./SaleProduct";
+import axios from 'axios';
 
 class SaleShop extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: ['鱼香肉丝','锅包肉'],
+      list: [],
       inputValue: '中午吃啥'
     };
     this.addList = this.addList.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.getJsonData = this.getJsonData.bind(this);
+  }
+  getJsonData(){
+    const that = this;
+    axios.get('https://www.easy-mock.com/mock/5cd52a04c385bc03ca2648f1/list')
+        .then(function (res) {
+          that.setState({
+            list:res.data.data
+          });
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
   }
   addList(){
-    this.setState({
-      list: [...this.state.list,this.state.inputValue],
-      inputValue: ''
-    })
+    if (this.state.inputValue !== '') {
+      this.setState({
+        list: [...this.state.list,this.state.inputValue],
+        inputValue: ''
+      })
+    }
   }
   deleteItem(index) {
-
+    let lists = this.state.list;
+    lists.splice(index,1);
+    this.setState({
+      list:lists
+    })
   }
   handleChange(e) {
     this.setState({
       inputValue: e.target.value
     })
+  }
+  componentWillMount() {
+    console.log("111组件将要渲染");
+  }
+  componentDidMount() {
+    console.log("111组件渲染完成");
+    this.getJsonData();
+  }
+  componentWillReceiveProps(nextProps, nextContext) {
+    console.log("111将要接收一个新的props");
+  }
+  componentWillUpdate(nextProps, nextState) {
+    //在组件接收到新的props或者state但还没有render时被调用
+    console.log('111在组件接收到新的props或者state但还没有render时被调用');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    //在组件完成更新后立即调用。在初始化时不会被调用。
+    console.log('111在组件完成更新后立即调用。在初始化时不会被调用。')
+  }
+  componentWillUnmount() {
+    //在组件从 DOM 中移除之前立刻被调用。
+    console.log('111在组件从 DOM 中移除之前立刻被调用。')
   }
   render() {
     return (
@@ -35,7 +79,17 @@ class SaleShop extends React.Component {
           <Button onClick={this.addList}>下单</Button>
         </div>
         <ul>
-
+          {
+            this.state.list.map((item, index) =>
+             <SaleProduct
+                 key={index+item}
+                 content={item}
+                 index={index}
+                 deleteItem={this.deleteItem}
+                 list={this.state.list}
+             />
+            )
+          }
         </ul>
       </React.Fragment>
     )
